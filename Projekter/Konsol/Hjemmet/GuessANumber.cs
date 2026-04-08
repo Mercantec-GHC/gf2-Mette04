7855a6f8f63af806d952184f8d8ef87315a3af85
Mette04 <mettevejling108@gmail.com>
2026-04-08 14:13:20 +0200

    08/04

--- Projekter/Konsol/Hjemmet/GuessANumber.cs
+++ Projekter/Konsol/Hjemmet/GuessANumber.cs
@@ -1,11 +1,70 @@
+using System.ComponentModel.Design;
+
 namespace Hjemmet
 {
     public class GuessANumber
     {
         public void Start()
         {
-            Console.WriteLine("Gæt et tal er ikke implementeret endnu.");
-            Console.ReadKey();
+            Console.WriteLine("Gæt et tal mellem 1 og 100 .");
+
+            Random random = new Random();
+            int randomNumber = random.Next(101);
+
+            Console.WriteLine("Indtast et tal");
+            
+            int point = 0;
+            string? guess;
+            int number;
+
+
+               
+                while (true)
+                {
+                    guess = Console.ReadLine();
+
+                    // Guard Clause for at det er et rigtigt tal
+                    if (int.TryParse(guess, out number))
+                    { 
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du skal skrive et tal.");
+                        continue;
+                    }
+
+                    // Guard Clause for tallet skal være mellem 1 og 100
+                    // contine starter loopet forfra
+
+                    if (number < 0 || number > 100)
+                    {
+                        Console.WriteLine("Nummeret skal være mellem 1 og 100");
+                        continue;
+                    }
+                
+                    point += 1;
+                    if (number == randomNumber)
+                    {
+                        Console.WriteLine("Du gjorde det!");
+                        Console.WriteLine("nummeret var " + randomNumber);
+                        Console.WriteLine("Du gættede det på " + point + " forsøg");
+
+                    }
+                    else if (number < randomNumber)
+                    {
+                        Console.WriteLine("Tallet er højere");
+
+                    }
+                    else if (number > randomNumber)
+                    {
+                        Console.WriteLine("Tallet er lavere");
+                    }
+
+
+
+                    
+                }
+                Console.ReadKey();
+            }  
         }
-    }
 }
--- Projekter/Konsol/Kontoret/BinaryConverter.cs
+++ Projekter/Konsol/Kontoret/BinaryConverter.cs
@@ -4,8 +4,44 @@ namespace Kontoret
     {
         public void Start()
         {
-            Console.WriteLine("Binærkodeomformer er ikke implementeret endnu.");
+            Console.WriteLine("Skriv et tal mellem 0 og 255");
+            int octet = int.Parse(Console.ReadLine());
+            // List of power of two numbers 128, 64, 32, 16, 8, 4, 2, 1
+            List<int> powerOfTwo = new List<int> { 128, 64, 32, 16, 8, 4, 2, 1 };
+            string binary = "";
+
+            for (int i = 0; i < powerOfTwo.Count; i++) {
+                
+                if (octet >= powerOfTwo[i])
+                {
+                    binary += "1";
+                    octet -= powerOfTwo[i];
+                }
+                else
+                {
+                    binary += "0";
+                }
+            }
+
+            Console.WriteLine(binary);
+
+
+            Console.WriteLine("Skriv en binær streng her!");
+
+            string? binaryInput = Console.ReadLine(); //10101010
+            int octetOutput = 0;
+
+            for (int i = 0; i < binaryInput.Length; i++) {
+                
+                if (binaryInput[i] == '1')
+                {
+                    octetOutput += powerOfTwo[i];
+                }               
+            }
+
+            Console.WriteLine(octetOutput);
             Console.ReadKey();
         }
+
     }
 }
