0c36ee66078620d35e80eff09daeb2e910a14e63
Mette04 <mettevejling108@gmail.com>
2026-04-27 14:24:06 +0200

    Update BinaryConverter.cs

--- Projekter/Konsol/Kontoret/BinaryConverter.cs
+++ Projekter/Konsol/Kontoret/BinaryConverter.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Kontoret
@@ -6,89 +9,197 @@ namespace Kontoret
     {
 
         public void Start()
-        {
-            
 
+
+        {
+            string? tal;
+            int svar;
+            string? num;
+            int ans;
+            
             Console.WriteLine("Skriv et tal mellem 0 og 255");
             // Fra decimal til binær
             IpAddress ipAddress = new IpAddress();
 
-            Console.WriteLine("Indtast den første octet: ");
-            ipAddress.octets.Add(int.Parse(Console.ReadLine()));
-            Console.WriteLine("Indtast den anden octet: ");
-            ipAddress.octets.Add(int.Parse(Console.ReadLine()));
-            Console.WriteLine("Indtast den tredje octet: ");
-            ipAddress.octets.Add(int.Parse(Console.ReadLine()));
-            Console.WriteLine("Indtast den fjerde octet: ");
-            ipAddress.octets.Add(int.Parse(Console.ReadLine()));
-
-
-            // List of power of two numbers 128, 64, 32, 16, 8, 4, 2, 1
-            List<int> powerOfTwo = new List<int> { 128, 64, 32, 16, 8, 4, 2, 1 };
-            string binary = "";
-
-            foreach(int octet in ipAddress.octets)
-            {
-                int tempoctet = octet;
-                for (int i = 0; i < powerOfTwo.Count; i++)
+            while (true) {
+                Console.WriteLine("Indtast den første octet: ");
+                tal = Console.ReadLine();
+                
+                //tjekkker at vi har skrevet en int/tal.
+                if (int.TryParse(tal, out svar))
                 {
-
-                    if (tempoctet >= powerOfTwo[i])
+                    //Tjekker om vores tal er mellem 0-255
+                    // Hvis ja så tilføjer den vores tal til vores liste
+                    if (svar > -1 && svar < 256)
                     {
-                        binary += "1";
-                        tempoctet -= powerOfTwo[i];
+                        ipAddress.octets.Add(svar);
                     }
                     else
                     {
-                        binary += "0";
+                        Console.WriteLine("Du skal skrive et tal mellem 0-255, start forfra.");
+                        //Clear vores liste, så vi kan starte forfra
+                        ipAddress.octets.Clear();
+                        //starter loopet forfra
+                        continue;
+                    }
+
+                }
+                else
+                {
+                    //Hvis ikke vi har intastet et tal kører den denne fejlkode.
+                    Console.WriteLine("Du skal skrive et tal, start forfra.");
+                    ipAddress.octets.Clear();
+                    continue;
+                }
+                
+                Console.WriteLine("Indtast den anden octet: ");
+                tal = Console.ReadLine();
+                if (int.TryParse(tal, out svar))
+                {
+                    if (svar > -1 && svar < 256)
+                    {
+                        ipAddress.octets.Add(svar);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du skal skrive et tal mellem 0-255, start forfra.");
+                        ipAddress.octets.Clear();
+                        continue;
                     }
                 }
-                binary += ".";
-            }
-
-
-            Console.WriteLine("Her er de i binær");
-            Console.WriteLine(binary);
-
-            Console.WriteLine("Indtast den første binære streng: ");
-            ipAddress.binær.Add(Console.ReadLine());
-            Console.WriteLine("Indtast den anden binære streng: ");
-            ipAddress.binær.Add(Console.ReadLine());
-            Console.WriteLine("Indtast den tredje binære streng: ");
-            ipAddress.binær.Add(Console.ReadLine());
-            Console.WriteLine("Indtast den fjerde binære streng: ");
-            ipAddress.binær.Add(Console.ReadLine());
-
-            
-            // Fra binær til decimal 
-
-
-            foreach(string binær in ipAddress.binær)
-            {
-                int octetOutput = 0;
-                for (int i = 0; i < binær.Length; i++) {
-                
-                    if (binær[i] == '1')
-                    {
-                        octetOutput += powerOfTwo[i];
-                    }               
+                else
+                {
+                    Console.WriteLine("Du skal skrive et tal, start forfra.");
+                    ipAddress.octets.Clear();
+                    continue;
                 }
-                ipAddress.octetOutput.Add(octetOutput);
-            }
+                Console.WriteLine("Indtast den tredje octet: ");
+                tal = Console.ReadLine();
+                if (int.TryParse(tal, out svar))
+                {
+                    if (svar > -1 && svar < 256)
+                    {
+                        ipAddress.octets.Add(svar);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du skal skrive et tal mellem 0-255, start forfra.");
+                        ipAddress.octets.Clear();
+                        continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Du skal skrive et tal, start forfra.");
+                    ipAddress.octets.Clear();
+                    continue;
+                }
+                Console.WriteLine("Indtast den fjerde octet: ");
+                tal = Console.ReadLine();
+                if (int.TryParse(tal, out svar))
+                {
+                    if (svar > -1 && svar < 256)
+                    {
+                        ipAddress.octets.Add(svar);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du skal skrive et tal mellem 0-255, start forfra.");
+                        ipAddress.octets.Clear();
+                        continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Du skal skrive et tal, start forfra.");
+                    ipAddress.octets.Clear();
+                    continue;
+                }
 
-            Console.WriteLine("Her er de i hele tal");
-            ipAddress.PrintOctetOutput();
-            Console.ReadKey();
+                //Den tilføjer vores nye streng og laver den til en int, og tilføjer den til vores liste, i dette tilfælde vores "octets" liste.
+
+
+
+                // List of power of two numbers 128, 64, 32, 16, 8, 4, 2, 1
+                List<int> powerOfTwo = new List<int> { 128, 64, 32, 16, 8, 4, 2, 1 };
+                string binary = "";
+
+                foreach (int octet in ipAddress.octets)
+                {
+
+                    //En tempoary/middlertidig octet
+                    int tempoctet = octet;
+                    for (int i = 0; i < powerOfTwo.Count; i++)
+                    {
+
+                        if (tempoctet >= powerOfTwo[i])
+                        {
+                            //hvis vores binære input er 1, så skal den gå op i vores poweroftwo liste, og tilføje tallet der passer til 1-tallets position
+                            binary += "1";
+                            tempoctet -= powerOfTwo[i];
+                        }
+                        else
+                        {
+                            //Hvis vore binære input er 0, skal den ikke tilføje noget.
+                            binary += "0";
+                        }
+                    }
+                    //Så vi kan få et punktum mellem hver octet 
+                    binary += ".";
+                }
+
+
+
+                Console.WriteLine("Her er de i binær");
+                Console.WriteLine(binary);
+
+                //Vi tager vores binære streng og tilføjer den til vores liste, i dette tilfælde vores binær liste.
+                Console.WriteLine("Indtast den første binære streng: ");
+                ipAddress.binær.Add(Console.ReadLine());
+                Console.WriteLine("Indtast den anden binære streng: ");
+                ipAddress.binær.Add(Console.ReadLine());
+                Console.WriteLine("Indtast den tredje binære streng: ");
+                ipAddress.binær.Add(Console.ReadLine());
+                Console.WriteLine("Indtast den fjerde binære streng: ");
+                ipAddress.binær.Add(Console.ReadLine());
+
+
+                // Fra binær til decimal 
+
+
+                foreach (string binær in ipAddress.binær)
+                {
+                    int octetOutput = 0;
+                    for (int i = 0; i < binær.Length; i++)
+                    {
+
+                        if (binær[i] == '1')
+                        {
+                            octetOutput += powerOfTwo[i];
+                        }
+                    }
+                    //Vi tilføjer octetOutput til vores liste octetOutput.
+                    ipAddress.octetOutput.Add(octetOutput);
+                }
+                Console.WriteLine("Her er de i hele tal");
+                ipAddress.PrintOctetOutput();               
+                Console.ReadKey();
+                break;
+            }
+                               
         }
 
+        //lister
         public class IpAddress
         {
+            //Octet er en værdi/streng, som foreksempel et tal fra 0-255 eller 8 binære tal, altså fra punktum til punktum
             public List<int> octets = new List<int>();
             public List<string> binær = new List<string>();
             public List<int> octetOutput = new List<int>();
 
             public void PrintOctetOutput()
             {
+                //Udskriver vores tal til sidst i konsollen, med punktum imellem.
                 Console.WriteLine($"{octetOutput[0]}.{octetOutput[1]}.{octetOutput[2]}.{octetOutput[3]}");
             }
         }
